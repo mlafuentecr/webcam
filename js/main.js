3d1d8a0de57e1523634f05f901fb63b032ec7852
mlafuentecr <mlafuente@gmail.com>
2020-09-15 14:00:53 -0600

    cam

---
+++ js/main.js
@@ -0,0 +1,14 @@
+   //Selector for your <video> element
+   const video = document.querySelector('#myVidPlayer');
+
+   //Core
+   window.navigator.mediaDevices.getUserMedia({ video: true })
+       .then(stream => {
+           video.srcObject = stream;
+           video.onloadedmetadata = (e) => {
+               video.play();
+           };
+       })
+       .catch( () => {
+           alert('You have give browser the permission to run Webcam and mic ;( ');
+       });
