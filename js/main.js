3abbb11bf3e2fc49a01fe1dcea24d3206612bbf3
mlafuentecr <mlafuente@gmail.com>
2020-09-16 09:38:53 -0600

    face2

--- js/main.js
+++ js/main.js
@@ -1,15 +1,19 @@
+import { config } from 'process';
    /* Import another CSS file */
    import '../styles.scss'
 
    //Selector for your <video> element
    const video = document.querySelector('#myVidPlayer');
    var front = false;
-   var constraints = { video: { facingMode: (front? "user" : "environment") } };
+   document.getElementById('flip-button').onclick = function() { 
+     front = !front;
+    console.log(front);
+    };
 
    //Core
    window.navigator.mediaDevices.getUserMedia({
      audio: false, 
-     video:  { facingMode: (front? "user" : "environment") } 
+     video:  { facingMode: (front ? "user" : "environment") } 
      })
        .then(stream => {
            video.srcObject = stream;
@@ -22,7 +26,7 @@
        });
 
 
-document.getElementById('flip-button').onclick = function() { front = !front; };
+
 
 
 if (navigator.mediaDevices.getSupportedConstraints().facingMode) {
